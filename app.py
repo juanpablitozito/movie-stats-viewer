3cf94adb77e545c224b9120a279d964cf4d0ca9e
Daniel <danielcarlos2135@gmail.com>
2026-03-17 23:09:52 -0300

    autenticando  o login acessando apenas por admin

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from datetime import timedelta 
+
+app = Flask(__name__)
+app.secret_key = "chave_secreta"
+app.permanent_session_lifetime = timedelta(days=30)
+
+usuario = "admin"
+senha = "123"
+
+# Adicionando outro usuário
+usuarios = [
+    {"usuario": "admin", "senha": "123"},
+    {"usuario": "daniel", "senha": "456"}
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    mensagem = ""
+
+    if request.method == 'POST':
+        user = request.form['usuario']
+        password = request.form['senha']
+
+        # Verifica se o usuário existe na lista
+        usuario_valido = None
+        for u in usuarios:
+            if u['usuario'] == user and u['senha'] == password:
+                usuario_valido = u['usuario']
+                break
+
+        if usuario_valido:
+            session['logged_in'] = True
+            session['usuario'] = usuario_valido
+            session.permanent = True
+            return redirect(url_for('index'))
+        else:
+            mensagem = "Usuário ou senha incorretos. Tente novamente."
+
+    return render_template('login.html', mensagem=mensagem)
+
+@app.route('/index')
+def index():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('index.html', usuario=session.get('usuario'))
+
+if __name__ == '__main__':
+    app.run( host="0.0.0.0", port=5000, debug=True)
