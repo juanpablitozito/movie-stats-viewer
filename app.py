9a7a54eee47283034f23e99fda3fc87650b91409
Juan <juanmartinsr07@gmail.com>
2026-03-26 17:24:47 -0300

    adicionando botão de chat para interação com robô I.A

--- app.py
+++ app.py
@@ -51,3 +51,8 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+    @app.route('/chat')
+    def chat():
+        return render_template('chat.html')
